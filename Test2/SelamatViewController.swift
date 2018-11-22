4dd81596b10b1a5452092ac4623168961d0f2c70
sekisah <37254707+sekisah@users.noreply.github.com>
2018-11-23 06:59:23 +0700

    Add files via upload

---
+++ Test2/SelamatViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SelamatViewController.swift
+//  Test2
+//
+//  Created by Sepotong Kisah on 11/22/18.
+//  Copyright © 2018 Sepotong Kisah. All rights reserved.
+//
+
+import UIKit
+
+class SelamatViewController: UIViewController {
+
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var selamatLabel: UILabel!
+    @IBOutlet weak var namaTextField: UITextField!
+    @IBOutlet weak var selesaiButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
